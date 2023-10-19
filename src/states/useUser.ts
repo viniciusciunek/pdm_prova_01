620236560b4b9dddf9f253bd04262af3dd3b1bd6
ciunekvg <vinicius.ciunek@virginia.com.br>
2023-10-19 20:42:04 -0300

    first

--- src/states/useUser.ts
+++ src/states/useUser.ts
@@ -1,5 +1,4 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
 type User = {
@@ -15,17 +14,17 @@ type Actions = {
   setUser: (user: User | null) => void;
 };
 
-const useUser = create<State & Actions>()(
-  persist(
-    (set) => ({
-      user: null,
-      setUser: (user: User | null) => set(() => ({ user })),
-    }),
-    {
-      name: "username-state-key-in-asyncstorage",
-      storage: createJSONStorage(() => AsyncStorage),
-    }
-  )
-);
+// const useUser = create<State & Actions>()(
+//   persist(
+//     (set) => ({
+//       user: null,
+//       setUser: (user: User | null) => set(() => ({ user })),
+//     }),
+//     {
+//       name: "username-state-key-in-asyncstorage",
+//       storage: createJSONStorage(() => AsyncStorage),
+//     }
+//   )
+// );
 
-export default useUser;
+// export default useUser;
